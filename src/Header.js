4819f5ba38a2892552a0fcd24de4b845269f1f3e
yuneko1127 <ono.yuneko.6@gmail.com>
2024-03-25 14:47:37 +0900

    fix and add data

--- src/Header.js
+++ src/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
         <div className="Header_nav">
             <nav>
                 <ul>
-                    <li><Link to="/">HOME</Link></li>
+                    <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                 </ul>
